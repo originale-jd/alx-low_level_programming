9eaaea5cf56fdc69ffaf2e024eca8457067b4f38
originale-jd <chopsticksrosh@example.com>
2023-03-16 14:46:12 -0700

    78

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: 0
- *
- */
+* You should produce the exact same output as in the sample
+* Warnings are allowed
+*
+* Your program should return 0
+*/
 int main(void)
 {
-char a;
-int b;
-long int c;
-long long int d;
-float f;
+  char c;
+  int i;
+  long li;
+  long long lli;
+  float f;
 
-printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d);
-printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return (0);
+  printf("Size of a char: %ld byte(s)\n", sizeof(c));
+  printf("Size of an int: %ld byte(s)\n", sizeof(i));
+  printf("Size of a long: %ld byte(s)\n", sizeof(li));
+  printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+  printf("Size of a float: %ld byte(s)\n", sizeof(f));
+  return (0);
 }
