caeaaba9b809f94e3f53cfe6a3d87c95d23b326c
originale-jd <chopsticksrosh@example.com>
2023-03-17 16:35:13 -0700

    344

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+
+	return (0);
+}
