7a5ebba93629d40e83e0eea5f97d48c1d3751131
originale-jd <chopsticksrosh@example.com>
2023-03-21 09:03:58 -0700

    3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet, in lowercase
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
