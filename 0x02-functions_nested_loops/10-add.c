c58e95d1ad24a773e81133023e40946ec1e135ab
originale-jd <chopsticksrosh@example.com>
2023-03-21 09:29:02 -0700

    gh

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @c: two integer arguments
+ * @m: two integer arguments
+ * Return: sum
+ */
+
+int add(int c, int m)
+{
+	return (c + m);
+}
